8bf549974e96779a481db81e93828e5dee8db07b
Kenan Ozbelli <kenan.ozbelli@gmail.com>
2021-01-28 15:04:17 -0500

    added more features on the annotated layout and configured server.js for the app-bridge

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ee9a911d08516829b265";
+/******/ 	var hotCurrentHash = "ed8d55099278a1f054cf";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/annotated_layout.js
+++ pages/annotated_layout.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import {Page, Layout, Card, Banner} from '@shopify/polaris';
+import {Page, Layout, Card, Banner, Button, Form, FormLayout, TextField, Stack, SettingToggle, TextStyle} from '@shopify/polaris';
 
 class AnnotatedLayout extends React.Component {
-    state = {};
+    state = {
+        discount: '10%',
+        enabled: false,
+    };
 
+    handleSubmit = () => {
+        this.setState({
+            discount: this.state.discount
+        });
+        console.log('submission', this.state);
+    }
+
+    handleChange = (name) => {
+        return (value) => this.setState({ [name]: value});
+    }
+    handleToogle = () => {
+        this.setState(({enabled}) => {
+            return { enabled: !enabled}
+        });
+    }
     render() {
+     
+       const { discount, enabled } = this.state;
+       const contentStatus = enabled ? 'Disabled' : 'Enabled';
+       const textStatus = enabled ? 'Enabled' : 'Disabeld';
         return (
             <Page>
                 <Layout>
@@ -17,13 +39,33 @@ class AnnotatedLayout extends React.Component {
                     </Layout.Section>
                     <Layout.AnnotatedSection title="Default Discount" description="Add a product to Sample App, it will automatically be discounted">
                         <Card sectioned>
-                            <div>Card</div>
+                            <Form onSubmit={this.handleSubmit}>
+                                <FormLayout>
+                                    <TextField value={discount} onChange={this.handleChange('discount')} label='Discount' type='discount'/>
+                                    <Stack distribution="trailing">
+                                        <Button primary submit>
+                                            Save
+                                        </Button>
+                                    </Stack>
+                                </FormLayout>
+                            </Form>
                         </Card>
+                    </Layout.AnnotatedSection>
+                    <Layout.AnnotatedSection title="Toogle Discount" description="Enable or Disable the discounts for your products">
+                        <SettingToggle
+                            action={{
+                                content: contentStatus,
+                                onAction: this.handleToogle,
+                            }}
+                            enabled={enabled}
+                        >
+                            this setting is {' '}
+                            <TextStyle variation='strong'>{textStatus}</TextStyle>
+                        </SettingToggle>
                     </Layout.AnnotatedSection>
                 </Layout>
             </Page>
         )
     }
 }
-
 export default AnnotatedLayout;
